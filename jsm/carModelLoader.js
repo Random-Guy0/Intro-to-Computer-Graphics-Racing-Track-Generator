0352335aa5ab90b306ad751242e977d2f893a693
Ronan Konstantin <ronankonstantin@gmail.com>
2022-05-08 15:10:43 +1000

    Changed car model + colour manipulation setup

--- jsm/carModelLoader.js
+++ jsm/carModelLoader.js
@@ -9,16 +9,46 @@ function addCarToScene (carSize, targetScene) {
     var mtlLoader = new MTLLoader()
     mtlLoader.load(CAR_MATERIAL_DIRECTORY, (materials) => {
         materials.preload()
+        materials.materials.color.color.set(0xFFBBBB)   // Colour of the car
+        materials.materials.disk.color.set(0xFFFFFF)    // Colour of the brake disk
+        //materials.materials.elements.color.set(0xFFFFFF)  // Front light, side mirrors, etc
+        materials.materials.tyre.color.set(0x000000)    // Colour of the Tyre
+        materials.materials.wheel.color.set(0xFFFFFF)    // Colour of the wheels
+        materials.materials.window.color.set(0xFFFFFF)    // Colour of the windows
+
         var loader = new OBJLoader()
         loader.setMaterials(materials)
         loader.load(CAR_MODEL_DIRECTORY, (obj) => {
-            var boundingBox = new THREE.Box3().setFromObject(obj)
+            obj.traverse (
+                (child) => {
+                        console.log(child)
+                }
+            )
+
+            for (var i = obj.children.length - 1; i >= 0; i--) {
+                //if(obj.children[i].name == 'Cube') obj.remove(obj.children[i])
+                //grila_Plane.002
+                //Plane.001_Plane
+                //car_Plane.001
+                switch(obj.children[i].name)
+                {
+                    case 'Cube':
+                    //case 'grila_Plane.002':
+                    case 'Plane.001_Plane':
+                    //case 'car_Plane.001':
+                        obj.remove(obj.children[i])
+                        break
+                    default:
+                }
+            }
         
+            var boundingBox = new THREE.Box3().setFromObject(obj)
             var center = new THREE.Vector3()
             var size = new THREE.Vector3()
             boundingBox.getCenter(center)
             boundingBox.getSize(size)
             var min = boundingBox.min
+            console.log(size.x + " " + size.y)
         
             var sca = new THREE.Matrix4()
             var tra = new THREE.Matrix4()
