ae6e4407395ae6134e361707e2715dd73a2d1cdf
Amren Gill <amren.gill@gmail.com>
2022-05-10 15:14:02 +1000

    switched to cannon.js

--- jsm/CarControls.js
+++ jsm/CarControls.js
@@ -1,8 +1,16 @@
+import * as THREE from "three";
+
+var moveDir = new THREE.Vector3(0, 0, 0);
+
 class CarControls
 {
-    constructor(car)
+    constructor(car, camera, moveSpeed, turnSpeed, THREE)
     {
         this.car = car;
+        this.camera = camera;
+
+        this.moveSpeed = moveSpeed;
+        this.turnSpeed = turnSpeed;
 
         window.addEventListener('keydown', this.onKeyDown);
     }
@@ -14,20 +22,39 @@ class CarControls
             case 87:
             case 38:
                 console.log("forward");
+                if(moveDir != null)
+                    moveDir.z -= 1;
                 break;
             case 83:
             case 40:
                 console.log("backwards");
+                if(moveDir != null)
+                    moveDir.z += 1;
                 break;
             case 65:
             case 37:
                 console.log("left");
+                if(moveDir != null)
+                    moveDir.x -= 1;
                 break;
             case 68:
             case 39:
                 console.log("right");
+                if(moveDir != null)
+                    moveDir.x += 1;
                 break;
         }
+    }
+    
+    createRigidBody()
+    {
+
+        return null;
+    }
+
+    move()
+    {
+        
     }
 }
 
